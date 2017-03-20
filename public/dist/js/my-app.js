47cea84f87be3ac3e8cbfcd12a65b799497f2c9b
yuan8 <yuan.putra@windowslive.com>
2017-03-21 03:18:45 +0700

    pages update

--- public/dist/js/my-app.js
+++ public/dist/js/my-app.js
@@ -9,5 +9,14 @@ var myApp = new Framework7({
 var $$ = Dom7;
 
 // // Add main view
-var mainView = myApp.addView('.view-main', {
+var mainView = myApp.addView('.view-main', {reloadPages:true
 });
+
+function popover_detail_project(ev) {
+	// console.log(ev);
+	var clickedLink = ev;
+	myApp.popover('.popover-detail-project', clickedLink);
+}
+
+
+
