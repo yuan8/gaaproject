43341e689aa5c7e36cee4363926ff8365094e193
yuan8 <yuan.putra@windowslive.com>
2017-03-24 01:16:27 +0700

    page edit

--- public/dist/js/my-app.js
+++ public/dist/js/my-app.js
@@ -32,15 +32,110 @@ myApp.onPageInit('detail-project', function (page) {
     var clickedLink = this;
     myApp.popover('.popover-detail-project', clickedLink);
 	});  
+	
 	closeFunctionPopover();
 
-	  $$('.open-left-panel').on('click', function (e) {
+	$$('.open-left-panel').on('click', function (e) {
         // 'left' position to open Left panel
         myApp.openPanel('left');
     });
 
+	// close panel left
+    $$('.panel-left a').on('click',function (e) {
+    	myApp.closePanel('left');
+    });
+
+
+
 
 });
+
+myApp.onPageInit('panel-design', function (page) {
+	$$('.open-left-panel').on('click', function (e) {
+	// 'left' position to open Left panel
+	myApp.openPanel('left');
+    });
+
+	// close panel left
+    $$('.panel-left a').on('click',function (e) {
+    	myApp.closePanel('left');
+    });
+
+	$$('.button-new-folder').on('click', function () {
+	    myApp.prompt('', 'CREATE NEW FOLDER', function (value) {
+	       
+	   });
+	});
+ 
+
+});
+
+myApp.onPageInit('detail-folder', function (page) {
+	$$('.button-popover-detail-folder').on('click', function () {
+    var clickedLink = this;
+    myApp.popover('.popover-detail-folder', clickedLink);
+	});  
+
+	$$('.open-left-panel').on('click', function (e) {
+	// 'left' position to open Left panel
+	myApp.openPanel('left');
+    });
+
+	// close panel left
+    $$('.panel-left a').on('click',function (e) {
+    	myApp.closePanel('left');
+    });
+
+	closeFunctionPopover();
+
+
+});
+
+
+myApp.onPageInit('project-update', function (page) {
+	$$('.button-popover-project-update').on('click', function () {
+    var clickedLink = this;
+    myApp.popover('.popover-project-update', clickedLink);
+	});  
+
+	$$('.open-left-panel').on('click', function (e) {
+	// 'left' position to open Left panel
+	myApp.openPanel('left');
+    });
+
+	// close panel left
+    $$('.panel-left a').on('click',function (e) {
+    	myApp.closePanel('left');
+    });
+
+	closeFunctionPopover();
+
+
+});
+
+
+
+myApp.onPageInit('budget', function (page) {
+	$$('.button-popover-project-update').on('click', function () {
+    var clickedLink = this;
+    myApp.popover('.popover-project-update', clickedLink);
+	});  
+
+	$$('.open-left-panel').on('click', function (e) {
+	// 'left' position to open Left panel
+	myApp.openPanel('left');
+    });
+
+	// close panel left
+    $$('.panel-left a').on('click',function (e) {
+    	myApp.closePanel('left');
+    });
+
+	closeFunctionPopover();
+
+
+});
+
 
 
 
