8bbb6af80352f85f99303f9b03f20e1b0a91a404
yuan8 <yuan.putra@windowslive.com>
2017-04-11 00:56:46 +0700

    check

--- FIX/dist/js/login.js
+++ FIX/dist/js/login.js
@@ -1,5 +1,10 @@
 
 var $$ = Dom7;
-$$.post('http://93.188.164.240/jsonrpc.php',{username:'jsonrpc',password:'d66377c2c65ee0d6113d2e8a6ab5f73ef0b525ea48aa0b2563fd5ea7263c'},function(data){
+
+var username=btoa('admin');
+var password=btoa('admin');
+console.log('after btoa = ' +username);
+
+$$.post('http://93.188.164.240/jsonrpc.php',{username:username,password:password},function(data){
 	console.log('data' +  data );
 })
